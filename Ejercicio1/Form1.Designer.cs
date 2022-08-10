57606122cd957ac68fde1f994e637b57b2cc75b3
DESKTOP-7MTEE4C\natal <nataly182000@gmail.com>
2022-08-10 17:17:33 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtvalor1 = new System.Windows.Forms.TextBox();
+            this.txtvalor2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicacion",
+            "Division"});
+            this.comboBox1.Location = new System.Drawing.Point(278, 182);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Operador";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Valor 2";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Resultado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtvalor1
+            // 
+            this.txtvalor1.Location = new System.Drawing.Point(279, 96);
+            this.txtvalor1.Name = "txtvalor1";
+            this.txtvalor1.Size = new System.Drawing.Size(179, 22);
+            this.txtvalor1.TabIndex = 5;
+            this.txtvalor1.TextChanged += new System.EventHandler(this.txtvalor1_TextChanged);
+            // 
+            // txtvalor2
+            // 
+            this.txtvalor2.Location = new System.Drawing.Point(278, 269);
+            this.txtvalor2.Name = "txtvalor2";
+            this.txtvalor2.Size = new System.Drawing.Size(179, 22);
+            this.txtvalor2.TabIndex = 6;
+            this.txtvalor2.TextChanged += new System.EventHandler(this.txtvalor2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.txtvalor2);
+            this.Controls.Add(this.txtvalor1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtvalor1;
+        private System.Windows.Forms.TextBox txtvalor2;
+    }
+}
+
---
+++ Ejercicio1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void respuesta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string opcion = comboBox1.Text;
+            double v1 = double.Parse(txtvalor1.Text);
+            double v2 = double.Parse(txtvalor2.Text);
+            double resultado = 0;
+
+            switch (opcion)
+            {
+                case "Suma":
+                    resultado = v1 + v2;
+                    MessageBox.Show("El resultado de la suma es: " + resultado);
+                    break;
+                case "Resta":
+                    resultado = v1 - v2;
+                    MessageBox.Show("El resultado de la resta es: " + resultado);
+                    break;
+                case "Multiplicacion":
+                    resultado = v1 * v2;
+                    MessageBox.Show("El resultado de la multiplicacion es: " + resultado);
+                    break;
+                case "Division":
+                    resultado = v1 / v2;
+                    MessageBox.Show("El resultado de la division es: " + resultado);
+                    break;
+
+            }
+        }
+
+        
+        private void txtvalor1_TextChanged(object sender, EventArgs e)
+        {
+            string num1;
+            num1 = txtvalor1.Text;
+
+            if(num1 == "")
+            {
+                MessageBox.Show("Debe ingresar un numero");
+            }
+         
+        }
+
+        private void txtvalor2_TextChanged(object sender, EventArgs e)
+        {
+            string num2;
+            num2 = txtvalor2.Text;
+
+            if (num2 == "")
+            {
+                MessageBox.Show("Debe ingresar un numero");
+            }
+
+        }
+    }
+
+}
